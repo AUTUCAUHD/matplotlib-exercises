2e2b74c77bdc2cb077f6af0877cc35ff7e3a12d0
SimpleDev <vladtheman46@gmail.com>
2025-12-23 22:18:21 -0500

    Create basic_line_plot.py

---
+++ basic-line-plot/basic_line_plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+years = np.array([2020, 2021, 2022, 2023, 2024])
+revenue = np.array([120, 150, 180, 160, 210])
+
+plt.plot(years, revenue)
+
+plt.title("Annual Revenue")
+
+plt.xlabel("Years")
+plt.ylabel("Revenue (by thousands)")
+
+plt.show()
